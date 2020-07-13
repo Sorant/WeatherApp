2eee5dab8fb744b9711a86f5a61ed8a7ded294d0
w48835wsiz <vladyslavzarubin@gmail.com>
2020-07-13 02:15:27 +0200

    Updates

---
+++ precache-manifest.ae82ba9ce557e2d7e725644853606726.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a59e6f01e3217761e8d3c8e1660f36f2",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "e44921890ff10b602fb1",
+    "url": "/weatherApp/static/css/main.dedb9dce.chunk.css"
+  },
+  {
+    "revision": "22edd1c93a1c56a61f91",
+    "url": "/weatherApp/static/js/2.feb06c3c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherApp/static/js/2.feb06c3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e44921890ff10b602fb1",
+    "url": "/weatherApp/static/js/main.f8372689.chunk.js"
+  },
+  {
+    "revision": "efba474b3fd5a4a7aa5f",
+    "url": "/weatherApp/static/js/runtime-main.d4919a9c.js"
+  },
+  {
+    "revision": "66823e823287d52ee6b938b7b84e247f",
+    "url": "/weatherApp/static/media/bg-img.66823e82.png"
+  },
+  {
+    "revision": "ef773e27ff441d92ac7ede664bb8acd5",
+    "url": "/weatherApp/static/media/clear.ef773e27.svg"
+  },
+  {
+    "revision": "06722e813ef6d26c2c609366f7b8384a",
+    "url": "/weatherApp/static/media/clouds.06722e81.svg"
+  },
+  {
+    "revision": "e34efc7ff57ada4f43ff60ac0ac1ad8e",
+    "url": "/weatherApp/static/media/error-illustration.e34efc7f.svg"
+  },
+  {
+    "revision": "0655a1391066350cf93cfdf7a427a4fb",
+    "url": "/weatherApp/static/media/humidity-icon.0655a139.svg"
+  },
+  {
+    "revision": "cfb91db4fccb536b6e36dc7fa33dcdc3",
+    "url": "/weatherApp/static/media/night-clear.cfb91db4.svg"
+  },
+  {
+    "revision": "ee650c5169b350d9058ab839fe92e422",
+    "url": "/weatherApp/static/media/night-clouds.ee650c51.svg"
+  },
+  {
+    "revision": "cf5ef5e9f3a2df117fb27535c5d3f7db",
+    "url": "/weatherApp/static/media/pressure-icon.cf5ef5e9.svg"
+  },
+  {
+    "revision": "ceba6257c2c02023e3f4277b3c2470c4",
+    "url": "/weatherApp/static/media/rain.ceba6257.svg"
+  },
+  {
+    "revision": "b686b4adbc879f9d578fff1fcdc681d5",
+    "url": "/weatherApp/static/media/snow.b686b4ad.svg"
+  },
+  {
+    "revision": "f6577dd27c736b16c9b1626a84da86fc",
+    "url": "/weatherApp/static/media/wind-icon.f6577dd2.svg"
+  }
+]);
